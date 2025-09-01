c435cdb9c6543fa03845f7b08ea6236845a357d3
Bhuvnesh Upadhyay <bhuvi819381@gmail.com>
2025-09-01 20:14:50 +0530

    another folder related bug fixed

--- lib/setup/project.sh
+++ lib/setup/project.sh
@@ -5,35 +5,30 @@ source "$(dirname "${BASH_SOURCE[0]}")/../utils/colors.sh"
 
 # Create Vite project with TypeScript
 create_vite_project() {
-    # Prevent 'undefined' directory issue
-    if [ -z "$PROJECT_NAME" ] || [ "$PROJECT_NAME" = "undefined" ]; then
-        USE_CURRENT_DIR=true
-    else
-        USE_CURRENT_DIR=false
+    # If no project name provided and not using current dir, ask user
+    if [ -z "$PROJECT_NAME" ] && [ "$USE_CURRENT_DIR" != true ]; then
+        read -p "Enter project name: " PROJECT_NAME
     fi
 
     if [ "$USE_CURRENT_DIR" = true ]; then
         print_status "Setting up Vite project with TypeScript in current directory using ${PACKAGE_MANAGER}..."
-
         if [ "$PACKAGE_MANAGER" = "pnpm" ]; then
-            pnpm create vite@latest . --template react-ts
+            pnpm create vite . --template react-ts
         else
             npm create vite@latest . --template react-ts
         fi
     else
         print_status "Creating Vite project with TypeScript using ${PACKAGE_MANAGER}..."
-
         if [ "$PACKAGE_MANAGER" = "pnpm" ]; then
-            pnpm create vite@latest "$PROJECT_NAME" --template react-ts
+            pnpm create vite "$PROJECT_NAME" --template react-ts
         else
             npm create vite@latest "$PROJECT_NAME" --template react-ts
         fi
-
         cd "$PROJECT_NAME" || exit
     fi
-
     print_success "Vite project created"
 }
+
 
 
 # Install dependencies
